804055a14f7c0ca38d60115d0e1095656236b7ca
Juan Antonio Hernández Pedreño <pedre@ct.provincia.eu>
2019-11-03 17:38:53 +0100

    (+) Video 40 tasks

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ const address = process.argv[2];
 
 
 if (address !== undefined) {
-    geocode(address, (error, data) => {
+    geocode(address, (error, {latitude, longitude, location}) => {
         if (error) {
             return console.log(error)
         }
-        forecast(data.latitude, data.longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
                 return console.log(error)
             }
-            console.log(data.location);
+            console.log(location);
             console.log(forecastData);
         });
     });
--- utils/forecast.js
+++ utils/forecast.js
@@ -4,16 +4,16 @@ const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/cccaf1967f8ecb26371b340a9adfb178/'+ latitude + ',' + longitude + '?units=si'
-    request({url: url, json: true}, (error, response) => {
+    request({url, json: true}, (error, {body}) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location!', undefined)
         } else {
             callback(undefined, {
-                summary: response.body.daily.data[0].summary,
-                temperature: response.body.currently.temperature,
-                chanceOfRain: response.body.currently.precipProbability
+                summary: body.daily.data[0].summary,
+                temperature: body.currently.temperature,
+                chanceOfRain: body.currently.precipProbability
             })
         }
     })
